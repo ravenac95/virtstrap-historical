5cffbe2c12e197c2c7d4f5dfde335f5a74b0bf39
Reuven V. Gonzales <reuven@tobetter.us>
2012-02-14 11:25:27 -0800

    Started setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys, os
+
+VERSION = '0.3.0-alpha'
+
+setup(name='virtstrap',
+    version=VERSION,
+    description="",
+    long_description="""\
+""",
+    classifiers=[],
+    keywords='',
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'virtualenv',
+        'virtstrap-core',
+    ],
+    entry_points={},
+)
